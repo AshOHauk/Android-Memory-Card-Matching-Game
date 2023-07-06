d0d868750bdbb2429fe4ce42b383d25c5b7a09e5
katherinengggg <e1112901@u.nus.edu>
2023-07-06 15:19:37 +0800

    testing

--- app/src/main/java/com/sa4108/draftca/MainActivity.java
+++ app/src/main/java/com/sa4108/draftca/MainActivity.java
@@ -21,7 +21,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-//hello
+
 
 public class MainActivity extends AppCompatActivity implements ImageDownloadCallback{
     private final Handler handler = new Handler();
